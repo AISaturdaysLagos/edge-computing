7c680dadfc2b7e3d7d080324b7b60d85882f6505
gigwegbe <gigwegbe@gmail.com>
2022-04-01 22:41:45 +0100

    update notebooks

--- docker_run.sh
+++
@@ -1,6 +0,0 @@
-sudo docker run --runtime nvidia -it --rm --network host \
-    -v /tmp/.X11-unix/:/tmp/.X11-unix \
-    -v /tmp/argus_socket:/tmp/argus_socket \
-    -v ~/my_apps:/dli/task/my_apps \
-    --device /dev/video0 \
-    nvcr.io/nvidia/dli/dli-nano-deepstream:v2.0.0-DS6.0.1
